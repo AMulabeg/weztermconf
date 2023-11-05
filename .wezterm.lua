b9ebed8c931e090e13692a49ae0e2fea93ab78b1
Amer Mulabegovic <amermulabegovic@gmail.com>
2023-11-05 17:15:20 +0100

    Started using wezterm as my main termainal :3

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,14 +7,17 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Apple Classic'
-config.font = wezterm.font 'JetBrains Mono'
+-- config.color_scheme = 'Apple Classic'
+
+config.color_scheme = "Sakura"
+
+config.font = wezterm.font("JetBrains Mono")
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = true
 config.window_decorations = "RESIZE"
@@ -22,4 +25,3 @@ config.window_decorations = "RESIZE"
 -- and finally, return the configuration to wezterm
 
 return config
-
